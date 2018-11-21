91adfd5501a166096237f2a73d24f011d90e31d9
maciec <maciekkuc1@gmail.com>
2018-11-21 23:09:38 +0100

    burgerMenu_scrap

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+let burgerButton = document.querySelector('.burger');
+let burgerMenu = document.querySelector('.menuItem');
 
+burgerButton.addEventListener('click', (e) => {
+	burgerMenu.style.display = 'grid';
+
+});
