7fa1ba6e819aedcf1d921d050c9f96a4b8e7d0c7
Maciej Kućmierowski <35165148+maciekKuc@users.noreply.github.com>
2019-01-12 18:26:33 +0100

    burger + slider_start
    Completed the burger menu css + js,
    Stared slider functionality

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-let burgerButton = document.querySelector('.burger');
-let burgerMenu = document.querySelector('.menuItem');
+const burgerButton = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.menuItem');
+const galleryImage = document.getElementById('galery-img');
+const closeImage = document.querySelector('.gallery-exit');
+const burgerClicked = false;
+
 
 burgerButton.addEventListener('click', (e) => {
-	burgerMenu.style.display = 'grid';
-
+	console.log(e.currentTarget);
+	if (burgerClicked === false){
+		burgerMenu.classList.add('burger-menu');
+		burgerMenu.classList.remove('menuItem', 'navUl');
+		document.querySelector('.burger > i').classList.remove('fa-bars');
+		document.querySelector('.burger > i').classList.add('fa-times');
+		//burgerMenu.style.display = 'grid';
+	}else{
+		//burgerMenu.style.display = 'none';
+		burgerMenu.classList.remove('burger-menu');
+		burgerMenu.classList.add('menuItem', 'navUl');
+		document.querySelector('.burger > i').classList.add('fa-bars');
+		document.querySelector('.burger > i').classList.remove('fa-times');
+	}
+	
+	burgerClicked = !burgerClicked;
 });
+
+
+closeImage.addEventListener('click', () => {
+	document.querySelector('.gallery').style.right = '100%';
+});
+
