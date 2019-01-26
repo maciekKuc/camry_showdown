a2571bba67200d03dd2bf814c35d823e5abab2af
Maciej Kućmierowski <35165148+maciekKuc@users.noreply.github.com>
2019-01-26 17:00:49 +0100

    burger menu fix

--- main.js
+++ main.js
@@ -6,27 +6,26 @@ const closeImage = document.querySelector('.gallery-exit');
 const rightButton = document.querySelector('.btn-right');
 const leftButton = document.querySelector('.btn-left');
 
-const burgerClicked = false;
-let picBumber = 0;
+let burgerClicked = false;
+let picNumber = 0;
 
 
 burgerButton.addEventListener('click', (e) => {
-	console.log(e.currentTarget);
-	if (burgerClicked === false){
-		burgerMenu.classList.add('burger-menu');
-		burgerMenu.classList.remove('menuItem', 'navUl');
-		document.querySelector('.burger > i').classList.remove('fa-bars');
-		document.querySelector('.burger > i').classList.add('fa-times');
-		//burgerMenu.style.display = 'grid';
-	}else{
-		//burgerMenu.style.display = 'none';
-		burgerMenu.classList.remove('burger-menu');
-		burgerMenu.classList.add('menuItem', 'navUl');
-		document.querySelector('.burger > i').classList.add('fa-bars');
-		document.querySelector('.burger > i').classList.remove('fa-times');
-	}
+				
+		document.querySelector('.burger > i').classList.toggle('fa-bars');
+		document.querySelector('.burger > i').classList.toggle('fa-times');
+
+		if (burgerClicked === false){
+			burgerMenu.classList.add('burger-menu');
+			burgerMenu.classList.remove('menuItem', 'navUl');
+			burgerMenu.style.display = 'grid';
+		}else{
+			burgerMenu.classList.remove('burger-menu');
+			burgerMenu.classList.add('menuItem', 'navUl');
+			burgerMenu.style.display = 'none';
+		}
 	
-	burgerClicked = !burgerClicked;
+	burgerClicked =! burgerClicked;
 });
 
 
@@ -40,29 +39,29 @@ const showGallery = (e) => {
 	document.querySelector('.gallery-content__img').src =  e.currentTarget.src;
 	for(let i = 0; i < imageList.length; i++){
 		if(imageList[i].src == document.querySelector('.gallery-content__img').src){
-			picBumber = i;
+			picNumber = i;
 		}
 	}
 }
 
 rightButton.addEventListener('click', () => {
-	if(picBumber >= imageList.length - 1){
-		picBumber = 0;
-		document.querySelector('.gallery-content__img').src = imageList[picBumber].src;
+	if(picNumber >= imageList.length - 1){
+		picNumber = 0;
+		document.querySelector('.gallery-content__img').src = imageList[picNumber].src;
 	} else{
-		picBumber += 1;
-		document.querySelector('.gallery-content__img').src = imageList[picBumber].src;
+		picNumber += 1;
+		document.querySelector('.gallery-content__img').src = imageList[picNumber].src;
 	}
 
 });
 
 leftButton.addEventListener('click', () => {
-	if(picBumber <= 0){
-		picBumber = imageList.length - 1;
-		document.querySelector('.gallery-content__img').src = imageList[picBumber].src;
+	if(picNumber <= 0){
+		picNumber = imageList.length - 1;
+		document.querySelector('.gallery-content__img').src = imageList[picNumber].src;
 	} else{
-		picBumber -= 1;
-		document.querySelector('.gallery-content__img').src = imageList[picBumber].src;
+		picNumber -= 1;
+		document.querySelector('.gallery-content__img').src = imageList[picNumber].src;
 	}	
 });
 
